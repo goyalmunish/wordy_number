1a5ede9104fb043d9c3c80539f11048d40ac80af
Munish Goyal <munish.goyal@universumglobal.com>
2016-07-16 12:43:17 +0530

    Build basic functionality to scan dictionary to find matches

--- lib/wordy_number.rb
+++ lib/wordy_number.rb
@@ -18,14 +18,15 @@ class WordyNumber
     number_of_words: 109582,
   }
 
-  attr_accessor :num, :dict
+  attr_accessor :num, :dict, :patterns
 
   # dictionary is expected to have one word per line
-  def initialize(user_number=nil, user_dictionary=DEFAULT_DICT_FILE_PATH)
-    self.num = user_number
+  def initialize(user_number="", user_dictionary=DEFAULT_DICT_FILE_PATH)
+    @num = user_number
     sanitize_number!
-    self.dict = user_dictionary
+    @dict = user_dictionary
     scan_dict!(dict)
+    @patterns = []
   end
 
   def dict_hash
@@ -39,13 +40,75 @@ class WordyNumber
     dh.keys.each{ |key| puts "#{key} -> #{dh[key].size}" }
   end
 
+  # TODO: before feeding the word here, we have to split it at the places of 1's
+  def find_all_matches(num_str=self.num, pattern_length=num_str.length)  # can the order cause issue here
+    patterns = []
+    if num_str.size == 0  # first deal with the edge cases
+      return patterns
+    end
+    if pattern_length == 1  # first deal with the edge cases
+      if dict_hash[pattern_length]
+        times = 0
+        dict_hash[pattern_length].each do |dict_word|
+          if num_str == dict_word
+            times += 1
+            patterns << dict_word.word_form
+          end
+        end
+        if times == 0
+          patterns << num_str
+        end
+      end
+    elsif pattern_length > 0  # general case
+      if dict_hash[pattern_length]
+        dict_hash[pattern_length].each do |dict_word|
+          matched_index = num_str =~ /#{dict_word.numeric_form}/
+          if matched_index
+            str1 = matched_index > 0 ? num_str[0..(matched_index - 1)] : ""
+            str2 = dict_word.word_form  # it has word_form, but str1 and str3 still have numeric_form
+            str3 = num_str[(matched_index + pattern_length)..-1]
+            patterns += self.class.concat_3_str_lists(find_all_matches(str1), [str2], find_all_matches(str3))
+          end
+        end
+      end
+      patterns += find_all_matches(num_str, pattern_length-1)
+    else
+      raise "NotReachableCode"
+    end
+
+    return patterns.uniq
+  end
+
+  def self.concat_3_str_lists(l_left, l_middle, l_right, separator="-")
+    # remove empty elements
+    [l_left, l_middle, l_right].each do |list|
+      list.delete_if{ |elem| elem.length == 0 }
+    end
+    # remove empty lists
+    lists = [l_left, l_middle, l_right].select{ |list| list.size > 0 }
+
+    # calculat product
+    result = case lists.size
+    when 3
+      lists[0].product(lists[1], lists[2]).map{ |elem| elem.join(separator) }
+    when 2
+      lists[0].product(lists[1]).map{ |elem| elem.join(separator) }
+    when 1
+      lists[0]
+    end
+
+    result
+  end
+
   private
 
   def sanitize_number!
-    #TODO: yet to implement
+    number_str = self.num
+    number_str.strip!
+    self.num = number_str
   end
 
-  def scan_dict!(dict_file_path = DEFAULT_DICT_FILE_PATH)
+  def scan_dict!(dict_file_path=DEFAULT_DICT_FILE_PATH)
     # reset dict_hash
     dh = dict_hash
     dh.clear
@@ -53,8 +116,9 @@ class WordyNumber
     # load dictionary
     File.open(dict_file_path, "r") do |file|
       file.each_line do |line|
-        dh[line.length] = [] unless dh[line.length]
-        dh[line.length] << DictWord.new(line)
+        dw = DictWord.new(line)
+        dh[dw.word_form.length] = [] unless dh[dw.word_form.length]  # `dw.word` is sanitized but `line` is not
+        dh[dw.word_form.length] << DictWord.new(line)
       end
     end
 
--- spec/wordy_number_spec.rb
+++ spec/wordy_number_spec.rb
@@ -15,6 +15,29 @@ describe WordyNumber do
     end
   end
 
+  describe ".concat_3_str_lists" do
+    it "returns product of 3 given lists" do
+      expect(WordyNumber.concat_3_str_lists(["111", "222", "333"], ["aa", "bb"], ["A", "A"]))
+      .to eq(["111-aa-A", "111-aa-A", "111-bb-A", "111-bb-A", "222-aa-A", "222-aa-A", "222-bb-A",
+              "222-bb-A", "333-aa-A", "333-aa-A", "333-bb-A", "333-bb-A"])
+      expect(WordyNumber.concat_3_str_lists([""], ["aa", "bb"], ["A", "A"]))
+      .to eq(["aa-A", "aa-A", "bb-A", "bb-A"])
+      expect(WordyNumber.concat_3_str_lists([""], ["aa", "bb"], [""]))
+      .to eq(["aa", "bb"])
+    end
+  end
+
+  describe "#find_all_matches" do
+    it "returns wordy patterns" do
+      expect(WordyNumber.new("225563").find_all_matches)
+      .to include(*%w(BALLO-3 BALK-ME BALL-OF CALL-ME CALL-63 2-ALL-OF BBL-JOE BBL-563 AB-5-JOE AB-KL-OF AC-5-LO-3 AC-55-OF AC-5563 CC-LL-MD 225-LO-3 2255-OF))
+      expect(WordyNumber.new("66473").find_all_matches)
+      .to include(*%w(MOIRE NOISE 6-MIRE NOIR-3 OOH-SE 66-HR-3 66-IS-3 NO-IS-3 MN-473 NO-4-RF 66473 664-RF))
+      expect(WordyNumber.new("8587071016").find_all_matches)
+      .to include(*["8-JUS-071016", "ULT-7071016", "85-UP-071016", "85-US-071016", "85-VS-071016", "8587071016"])
+    end
+  end
+
   describe "#dict_hash" do
     subject { WordyNumber.new }
     it "returns non-empty hash with number as keys" do
